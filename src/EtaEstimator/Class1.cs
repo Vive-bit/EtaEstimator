eeeef9ae0593a9fb1f1c96a3c5c70c87ca37437a
Vive-bit <martinlechner210@gmail.com>
2025-08-18 19:33:04 +0200

    init

---
+++ src/EtaEstimator/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace EtaEstimator;
+
+public class Class1
+{
+
+}
---
+++ tests/EtaEstimator.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace EtaEstimator.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
